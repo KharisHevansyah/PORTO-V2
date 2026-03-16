002ae7f69372b1ef7e8d84a9983ed8d5b8f56985
KharisHevansyah <92216150+KharisHevansyah@users.noreply.github.com>
2026-03-17 05:49:32 +0700

    Update toggle.js

--- api/toggle.js
+++ api/toggle.js
@@ -1,9 +1,13 @@
-const { kv } = require('@vercel/kv');
+const { Redis } = require('@upstash/redis');
+const redis = new Redis({
+  url: process.env.hevansyah_KV_REST_API_URL,
+  token: process.env.hevansyah_KV_REST_API_TOKEN,
+});
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') return res.status(405).end();
   const { id, tampilkan, password } = req.body;
   if (password !== process.env.ADMIN_PASSWORD) return res.status(403).json({ error: 'Unauthorized' });
-  await kv.hset(`work:${id}`, { tampilkan: tampilkan ? 'true' : 'false' });
+  await redis.hset('work:' + id, { tampilkan: tampilkan ? 'true' : 'false' });
   res.status(200).json({ success: true });
 };
