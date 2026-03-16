20e7e55cb1fb85b4ba1f65e7e75a00bda7236491
KharisHevansyah <92216150+KharisHevansyah@users.noreply.github.com>
2026-03-17 05:48:08 +0700

    Update delete.js

--- api/delete.js
+++ api/delete.js
@@ -1,10 +1,14 @@
-const { kv } = require('@vercel/kv');
+const { Redis } = require('@upstash/redis');
+const redis = new Redis({
+  url: process.env.hevansyah_KV_REST_API_URL,
+  token: process.env.hevansyah_KV_REST_API_TOKEN,
+});
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') return res.status(405).end();
   const { id, password } = req.body;
   if (password !== process.env.ADMIN_PASSWORD) return res.status(403).json({ error: 'Unauthorized' });
-  await kv.del(`work:${id}`);
-  await kv.lrem('portfolio:works', 0, id);
+  await redis.del('work:' + id);
+  await redis.lrem('portfolio:works', 0, id);
   res.status(200).json({ success: true });
 };
